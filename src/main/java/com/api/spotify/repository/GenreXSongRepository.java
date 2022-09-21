6ab682a700c1e5e192499b9e0f0a7fb429b48990
Gafhito <gonzalo.ferrari@outlook.es>
2022-09-21 00:06:22 -0300

    Actualización de Repositorys de tablas intermedias, falta una...

--- src/main/java/com/api/spotify/repository/GenreXSongRepository.java
+++ src/main/java/com/api/spotify/repository/GenreXSongRepository.java
@@ -5,6 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface GenreXSongRepository extends JpaRepository<GenreXSong, Long> {
+
+    // Este Repository funciona ok...
+
+    @Query(value = "select gxs.id, gxs.genre, gxs.song from genre_x_song as gxs", nativeQuery = true)
+    List<GenreXSong> findAll();
+
+    @Query(value = "select gxs.id, gxs.genre, gxs.song from genre_x_song as gxs where gxs.id = ?1", nativeQuery = true)
+    Optional<GenreXSong> findById(Long id);
+
 }
--- src/main/java/com/api/spotify/repository/PlaylistXSongRepository.java
+++ src/main/java/com/api/spotify/repository/PlaylistXSongRepository.java
@@ -2,8 +2,21 @@ package com.api.spotify.repository;
 
 import com.api.spotify.entity.PlaylistXSong;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PlaylistXSongRepository extends JpaRepository<PlaylistXSong, Long> {
+
+    // Este Repository NO funciona (?
+
+    @Query(value = "select pxs.id, pxs.song, pxs.playlist from playlist_x_song pxs", nativeQuery = true)
+    List<PlaylistXSong> findAll();
+
+    @Query(value = "select pxs.id, pxs.song, pxs.playlist from playlist_x_song pxs where pxs.id = ?1", nativeQuery = true)
+    Optional<PlaylistXSong> findById(Long id);
+
 }
